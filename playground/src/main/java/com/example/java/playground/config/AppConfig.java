39e06ebb9b2e1440cd595ce4994b9000d0404220
joao.luzio <joao.luzio@gmail.com>
2020-05-24 20:35:40 +0100

    Examples

---
+++ playground/src/main/java/com/example/java/playground/config/AppConfig.java
@@ -0,0 +1,19 @@
+package com.example.java.playground.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new Jackson2ObjectMapperBuilder()
+                .modules(new JavaTimeModule())
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
+}
