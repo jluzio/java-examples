87872c7544666936a42e28837aae039f5271c8cb
João Luzio <joao.luzio@gmail.com>
2020-08-26 12:38:46 +0100

    AtomicReferenceTest

---
+++ playground/src/test/java/com/example/java/playground/features/AtomicReferenceTest.java
@@ -0,0 +1,22 @@
+package com.example.java.playground.features;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class AtomicReferenceTest {
+
+  @Test
+  public void test() {
+    AtomicReference<Integer> ref = new AtomicReference<>(0);
+    Stream.of(1, 2, 5, 6, 2).forEach(v -> {
+      if (ref.get() < v) {
+        ref.set(v);
+      }
+    });
+    log.info("max: {}", ref.get());
+  }
+
+}
