702f9768954f83c8cbb42b14c82032618b80581e
joao.luzio <joao.luzio@gmail.com>
2020-11-06 02:45:39 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/rxjava/RxJavaTest.java
+++ playground/src/test/java/com/example/java/playground/rxjava/RxJavaTest.java
@@ -2,6 +2,7 @@ package com.example.java.playground.rxjava;
 
 import com.example.java.playground.AbstractTest;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,8 @@ public class RxJavaTest extends AbstractTest {
 
   @Test
   void test() {
-    Flowable.just("Hello world").subscribe(System.out::println);
+    Flowable.just("Hello world")
+        .subscribe(System.out::println);
 
     Flowable.range(1, 10)
         .subscribe(v -> log.info("[simple]value: {}", v));
@@ -18,5 +20,18 @@ public class RxJavaTest extends AbstractTest {
         .observeOn(Schedulers.computation())
         .map(v -> v * v)
         .subscribe(v -> log.info("[compute]value: {}", v));
+  }
+
+  @Test
+  void error_handling() {
+    Observable.range(-4, 10)
+        .map(i -> 100 / i)
+        .subscribe(System.out::println, t -> System.out.println("some error happened"));
+
+    Observable.range(-4, 10)
+        .map(i -> 100 / i)
+        .onErrorReturnItem(-1)
+        .subscribe(System.out::println, t -> System.out.println("some error happened"));
+
   }
 }
