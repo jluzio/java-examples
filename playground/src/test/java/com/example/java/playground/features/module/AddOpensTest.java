7dd8048ed9d202ed6fd1056a5bf512ad5764945f
João Luzio <joao.luzio@gmail.com>
2022-05-19 19:37:56 +0100

    Examples

---
+++ playground/src/test/java/com/example/java/playground/features/module/AddOpensTest.java
@@ -0,0 +1,19 @@
+package com.example.java.playground.features.module;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class AddOpensTest {
+
+  record Data(String id, String name) {}
+
+  @Test
+  void test() {
+    var data = new Data("id1", "name1");
+    var target = List.of(data);
+    log.debug("{}", ReflectionToStringBuilder.toString(target));
+  }
+}
