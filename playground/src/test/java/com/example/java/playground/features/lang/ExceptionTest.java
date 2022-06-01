39b439ed19876195b9a3438f944557780a9f4bd9
João Luzio <joao.luzio@gmail.com>
2022-06-01 18:36:57 +0100

    Examples

---
+++ playground/src/test/java/com/example/java/playground/features/lang/ExceptionTest.java
@@ -0,0 +1,35 @@
+package com.example.java.playground.features.lang;
+
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ExceptionTest {
+
+  @Test
+  void initCause() {
+    var cause = new IllegalArgumentException("Some runtime exception");
+    var throwable = new IndexOutOfBoundsException("Index was 0").initCause(cause);
+    log.debug("throwable", throwable);
+    Assertions.assertThatThrownBy(
+            () -> {
+              throw throwable;
+            })
+        .isInstanceOf(IndexOutOfBoundsException.class)
+        .hasCauseInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void initCause_invalid() {
+    var cause = new IllegalArgumentException("Some runtime exception");
+    var anotherCause = new UnsupportedOperationException("Another runtime exception");
+    var throwable = new IOException("Index was 0", cause);
+    Assertions.assertThatThrownBy(
+            () -> {
+              throw throwable.initCause(anotherCause);
+            })
+        .isInstanceOf(IllegalStateException.class);
+  }
+}
