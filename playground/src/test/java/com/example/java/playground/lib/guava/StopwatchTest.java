742f40d9171b08ef8ae904d1b0102c82b6ff9cd1
João Luzio <joao.luzio@gmail.com>
2022-10-28 18:17:50 +0100

    Examples

---
+++ playground/src/test/java/com/example/java/playground/lib/guava/StopwatchTest.java
@@ -0,0 +1,23 @@
+package com.example.java.playground.lib.guava;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.base.Stopwatch;
+import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+class StopwatchTest {
+
+  @Test
+  void test() {
+    var stopwatch = Stopwatch.createStarted();
+    Mono.delay(Duration.ofMillis(100)).block();
+    var elapsed = stopwatch.stop().elapsed();
+    assertThat(elapsed).isGreaterThanOrEqualTo(Duration.ofMillis(100));
+    log.debug("elapsed: {}", stopwatch);
+  }
+
+}
