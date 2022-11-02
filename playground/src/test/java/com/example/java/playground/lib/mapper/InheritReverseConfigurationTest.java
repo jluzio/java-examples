b7d20cf4085a6ac414200ee83187e8cf82ce1d77
João Luzio <joao.luzio@gmail.com>
2022-11-02 14:59:28 +0000

    Examples

---
+++ playground/src/test/java/com/example/java/playground/lib/mapper/InheritReverseConfigurationTest.java
@@ -0,0 +1,41 @@
+package com.example.java.playground.lib.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.types.User;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+class InheritReverseConfigurationTest {
+
+  record AnotherUser(String anotherUsername, String anotherFullName) {
+
+  }
+
+  @Mapper
+  interface InheritReverseConfigurationMapper {
+
+    InheritReverseConfigurationMapper INSTANCE = Mappers.getMapper(
+        InheritReverseConfigurationMapper.class);
+
+    @Mapping(target = "username", source = "anotherUsername")
+    @Mapping(target = "fullName", source = "anotherFullName")
+    User toUser(AnotherUser anotherUser);
+
+    @InheritInverseConfiguration
+    AnotherUser toAnotherUser(User user);
+  }
+
+  @Test
+  void test() {
+    var mapper = InheritReverseConfigurationMapper.INSTANCE;
+    assertThat(mapper.toAnotherUser(new User()
+        .withUsername("username1")
+        .withFullName("fullName1")))
+        .isEqualTo(new AnotherUser("username1", "fullName1"));
+  }
+
+}
