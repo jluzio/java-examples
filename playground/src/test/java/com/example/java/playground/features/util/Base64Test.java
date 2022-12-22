29b1baafeb3e5d80e01c6c1c5c6c6e76413152c3
João Luzio <joao.luzio@gmail.com>
2022-12-22 15:18:09 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/features/util/Base64Test.java
+++ playground/src/test/java/com/example/java/playground/features/util/Base64Test.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.base.Strings;
 import java.nio.file.Files;
 import java.util.Base64;
+import java.util.Base64.Decoder;
 import java.util.Map;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
@@ -69,6 +70,33 @@ class Base64Test {
         "eq encodedDataMime", Objects.equals(encodedData, encodedDataMime),
         "eq encodedDataUrl", Objects.equals(encodedData, encodedDataUrl)
     ));
+  }
+
+  @Test
+  void test_data_with_header() {
+    String data = "iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAr0lEQVQYV22PIQ6FMBBEpwrZpBeo4QYkCK5AgsJgEJVcAYfmEhg4AZYT9AgEiWuTCgSpWNImfMNftcnM7L5h8zzTuq6o6xp5nuOd+75hrQXruo6UUliWBeM4Rt05h33fIYQAq6qKsiyDMQbDMERDENM0xXVdYH3fU1EU2LYNTdNEQxA55zjPE6xtW5qmKabLsvwapJQUAL33/18cx0GB9j37gdRaU6BNkuRXMSxvzQf0FFu5VGl5DQAAAABJRU5ErkJggg==";
+    String dataWithHeader = "data:image/png;base64," + data;
+    testDecoder(Base64.getDecoder(), "Base64.getDecoder()",
+        data.getBytes(), dataWithHeader.getBytes());
+    testDecoder(Base64.getUrlDecoder(), "Base64.getUrlDecoder()",
+        data.getBytes(), dataWithHeader.getBytes());
+    testDecoder(Base64.getMimeDecoder(), "Base64.getMimeDecoder()",
+        data.getBytes(), dataWithHeader.getBytes());
+  }
+
+  private void testDecoder(Decoder decoder, String decoderId, byte[] data, byte[] dataWithHeader) {
+    try {
+      byte[] decodedData = decoder.decode(data);
+      log.debug("decodedData.length = {}", decodedData.length);
+    } catch (Exception e) {
+      log.debug("Does not decode basic data with {}", decoderId);
+    }
+    try {
+      byte[] decodedDataWithHeader = decoder.decode(dataWithHeader);
+      log.debug("decodedDataWithHeader.length = {}", decodedDataWithHeader.length);
+    } catch (Exception e) {
+      log.debug("Does not decode data with header with {}", decoderId);
+    }
   }
 
 }
