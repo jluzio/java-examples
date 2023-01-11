efffb11d2b8320460abf58d21f1f60070922bf8f
João Luzio <joao.luzio@gmail.com>
2023-01-11 18:04:56 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/lib/guava/CollectTest.java
+++ playground/src/test/java/com/example/java/playground/lib/guava/CollectTest.java
@@ -3,11 +3,15 @@ package com.example.java.playground.lib.guava;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class CollectTest {
 
   @Test
@@ -39,6 +43,18 @@ class CollectTest {
         "k5", "v5");
     assertThat(new ArrayList<>(mapDefault.keySet()))
         .isNotEqualTo(List.of("k1", "k2", "k3", "k4", "k5"));
+  }
+
+  @Test
+  void forEachPair() {
+    var output = new ArrayList<String>();
+    var values = List.of("a", "b", "c");
+    Streams.forEachPair(
+        values.stream(),
+        IntStream.range(0, values.size()).boxed(),
+        (v, i) -> output.add("%s:%s".formatted(i, v)));
+    log.debug("output: {}", output);
+    assertThat(output).contains("0:a", "1:b", "2:c");
   }
 
 }
