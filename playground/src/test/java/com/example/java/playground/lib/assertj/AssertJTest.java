735f39df00ecb7fcb65caa28484f2292db5e0a97
João Luzio <joao.luzio@gmail.com>
2023-03-03 18:35:09 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/lib/assertj/AssertJTest.java
+++ playground/src/test/java/com/example/java/playground/lib/assertj/AssertJTest.java
@@ -1,11 +1,16 @@
 package com.example.java.playground.lib.assertj;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 class AssertJTest {
 
   record Todo(String id, String task, LocalDate startDate, LocalDate endDate) {
@@ -27,6 +32,29 @@ class AssertJTest {
         );
     assertThat(todos.get(0))
         .satisfies(v -> assertThat(v.endDate()).isNull());
+  }
+
+  @Test
+  void exceptions() {
+    assertThatCode(() -> {
+    })
+        .doesNotThrowAnyException();
+
+    assertThatNoException()
+        .isThrownBy(() -> {
+        });
+
+    assertThatCode(() -> {
+      throw new IllegalArgumentException("Bad data");
+    })
+        .isInstanceOf(IllegalArgumentException.class)
+        .satisfies(e -> log.info("Thrown", e));
+
+    assertThatThrownBy(() -> {
+      throw new IllegalArgumentException("Bad data");
+    })
+        .isInstanceOf(IllegalArgumentException.class)
+        .satisfies(e -> log.info("Thrown", e));
   }
 
 }
