f293049212f02e419d9518376fcf610c730bc93d
João Luzio <joao.luzio@gmail.com>
2023-03-14 14:00:02 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/features/util/LocaleTest.java
+++ playground/src/test/java/com/example/java/playground/features/util/LocaleTest.java
@@ -3,6 +3,7 @@ package com.example.java.playground.features.util;
 import com.google.common.base.Strings;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.Locale.IsoCountryCode;
 import java.util.MissingResourceException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -11,18 +12,30 @@ import org.junit.jupiter.api.Test;
 class LocaleTest {
 
   @Test
-  void getAvailableLocales() {
-    Arrays.stream(Locale.getAvailableLocales()).forEach(locale -> {
-      log.info(Strings.repeat("-", 80));
-      log.info("locale: {}", locale);
-      log.info("country: {}", locale.getCountry());
-      try {
-        log.info("iso3Country: {}", locale.getISO3Country());
-      } catch (MissingResourceException e) {
-        log.info("iso3Country: missing :: {}", e.getMessage());
-      }
-      log.info("language: {}", locale.getLanguage());
-    });
+  void locale_info() {
+    Arrays.stream(Locale.getAvailableLocales()).forEach(this::localeInfo);
+
+    var customLocale = Locale.of("PT", "PT");
+    localeInfo(customLocale);
+  }
+
+  @Test
+  void getISOCountries() {
+    log.info("{}", Locale.getISOCountries(IsoCountryCode.PART1_ALPHA2));
+    log.info("{}", Locale.getISOCountries(IsoCountryCode.PART1_ALPHA3));
+    log.info("{}", Locale.getISOCountries(IsoCountryCode.PART3));
+  }
+
+  void localeInfo(Locale locale) {
+    log.info(Strings.repeat("-", 80));
+    log.info("locale: {}", locale);
+    log.info("country: {}", locale.getCountry());
+    try {
+      log.info("iso3Country: {}", locale.getISO3Country());
+    } catch (MissingResourceException e) {
+      log.info("iso3Country: missing :: {}", e.getMessage());
+    }
+    log.info("language: {}", locale.getLanguage());
   }
 
 }
