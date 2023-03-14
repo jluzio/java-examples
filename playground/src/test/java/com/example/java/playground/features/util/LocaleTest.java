05e48af9168045c4cbebf76a870a996c9c586c6a
João Luzio <joao.luzio@gmail.com>
2023-03-14 13:50:14 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/features/util/LocaleTest.java
+++ playground/src/test/java/com/example/java/playground/features/util/LocaleTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 class LocaleTest {
 
   @Test
-  void locales() {
+  void getAvailableLocales() {
     Arrays.stream(Locale.getAvailableLocales()).forEach(locale -> {
       log.info(Strings.repeat("-", 80));
       log.info("locale: {}", locale);
