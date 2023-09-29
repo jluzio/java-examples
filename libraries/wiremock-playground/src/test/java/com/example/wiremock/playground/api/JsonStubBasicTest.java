9a136be66db55d806b4390ce1c4051dbfc4b073b
João Luzio <joao.luzio@gmail.com>
2023-09-29 23:11:22 +0100

    Updated versions

--- libraries/wiremock-playground/src/test/java/com/example/wiremock/playground/api/JsonStubBasicTest.java
+++ libraries/wiremock-playground/src/test/java/com/example/wiremock/playground/api/JsonStubBasicTest.java
@@ -8,10 +8,8 @@ import com.maciejwalkowiak.wiremock.spring.EnableWireMock;
 import com.maciejwalkowiak.wiremock.spring.InjectWireMock;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.core.env.Environment;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
