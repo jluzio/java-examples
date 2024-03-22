f2d1d3d3298068462e53adb24c8218c887d993f3
João Luzio <joao.luzio@gmail.com>
2024-03-22 14:39:12 +0000

    Examples

---
+++ libraries/protobuf/src/test/java/com/example/types/UserProtosTest.java
@@ -0,0 +1,38 @@
+package com.example.types;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class UserProtosTest {
+
+  @Test
+  void write_and_read() throws IOException {
+    var user = UserProtos.User.newBuilder()
+        .setId(1)
+        .setName("John Doe")
+        .setUsername("john.doe@server.org")
+        .setAddress(UserProtos.Address.newBuilder()
+            .setCountry("PT")
+            .setCity("City")
+            .setStreet("Street")
+            .build())
+        .build();
+    log.info("user: {}", user);
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    user.writeTo(outputStream);
+    log.info("user output: {}", outputStream);
+
+    var parsedUser = UserProtos.User.parseFrom(outputStream.toByteArray());
+    log.info("parsedUser: {}", parsedUser);
+
+    assertThat(user)
+        .isEqualTo(parsedUser);
+  }
+
+}
