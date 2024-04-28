f5b7c67f26035e8d6a5e92e278427ddca2b0b61c
João Luzio <joao.luzio@gmail.com>
2024-04-28 15:16:52 +0100

    Examples

--- training/challenges/src/test/java/com/example/java/challenges/JsonTesting.java
+++ training/challenges/src/test/java/com/example/java/challenges/JsonTesting.java
@@ -1,8 +1,6 @@
 package com.example.java.challenges;
 
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -10,47 +8,48 @@ import java.util.List;
 import java.util.Scanner;
 
 public class JsonTesting {
-    public static void main(String[] args) {
-        try {
-            throw new RuntimeException("");
-        } catch (Exception e) {
-            System.out.println("caught");
-        }
+
+  public static void main(String[] args) {
+    try {
+      throw new RuntimeException("");
+    } catch (Exception e) {
+      System.out.println("caught");
     }
+  }
 
 
-    /*
-     * Complete the 'getRecordsByAgeGroup' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER ageStart
-     *  2. INTEGER ageEnd
-     *  3. INTEGER bpDiff
-     *
-     *  https://jsonmock.hackerrank.com/api/medical_records
-     */
+  /*
+   * Complete the 'getRecordsByAgeGroup' function below.
+   *
+   * The function is expected to return an INTEGER_ARRAY.
+   * The function accepts following parameters:
+   *  1. INTEGER ageStart
+   *  2. INTEGER ageEnd
+   *  3. INTEGER bpDiff
+   *
+   *  https://jsonmock.hackerrank.com/api/medical_records
+   */
 
-    public static List<Integer> getRecordsByAgeGroup(int ageStart, int ageEnd, int bpDiff) {
-        try {
-            URL url = new URL(String.format("https://jsonmock.hackerrank.com/api/medical_records"));
+  public static List<Integer> getRecordsByAgeGroup(int ageStart, int ageEnd, int bpDiff) {
+    try {
+      URL url = new URL(String.format("https://jsonmock.hackerrank.com/api/medical_records"));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    public static void getPage(int pageNum) {
-        try (Scanner scanner = new Scanner(new URL("https://jsonmock.hackerrank.com/api/medical_records").openStream(),
-                StandardCharsets.UTF_8.toString()))
-        {
-            String json = scanner.nextLine();
-            System.out.println(json);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void getPage(int pageNum) {
+    try (Scanner scanner = new Scanner(
+        new URL("https://jsonmock.hackerrank.com/api/medical_records").openStream(),
+        StandardCharsets.UTF_8.toString())) {
+      String json = scanner.nextLine();
+      System.out.println(json);
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
