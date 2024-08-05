8f48c99846d0949ed42cc26ad8143e4cbe13c004
João Luzio <joao.luzio@gmail.com>
2024-08-05 14:26:28 +0100

    Examples

--- playground/src/test/java/com/example/java/playground/features/stream/CollectorsTest.java
+++ playground/src/test/java/com/example/java/playground/features/stream/CollectorsTest.java
@@ -3,14 +3,18 @@ package com.example.java.playground.features.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.Test;
 
-@Slf4j
+@Log4j2
 class CollectorsTest {
 
   @Test
@@ -104,6 +108,72 @@ class CollectorsTest {
         .isEqualTo(Map.of(
             "a", List.of("a1234", "a123", "a12345"),
             "b", List.of("b1", "b123")));
+  }
+
+  @Test
+  void joining() {
+    var employees = employees();
+
+    var employeeNamesSortedByName = employees.stream()
+        .map(Employee::name)
+        .sorted()
+        .collect(Collectors.joining(","));
+    log.info(employeeNamesSortedByName);
+    assertThat(employeeNamesSortedByName)
+        .isEqualTo("Ana,Ben,John,Mary,Peter,Reggie,Sam,Samantha,Ted,Valery");
+  }
+
+  @Test
+  void groupBy_counting() {
+    var employees = employees();
+
+    var employeeCountGroupedByDepartment = employees.stream()
+        .collect(Collectors.groupingBy(Employee::department, Collectors.counting()));
+    log.info(employeeCountGroupedByDepartment);
+    assertThat(employeeCountGroupedByDepartment)
+        .isEqualTo(Map.of(
+            "DM", 2L,
+            "HR", 2L,
+            "IT", 6L
+        ));
+  }
+
+  @Test
+  void toMap_customMap() {
+    var employees = employees();
+
+    Comparator<Employee> employeeComparator = Comparator.comparingInt(Employee::id);
+    var employeeByIdMap = employees.stream()
+        .collect(Collectors.toMap(
+            employee -> employee,
+            Employee::id,
+            (v1, v2) -> v1,
+            () -> new TreeMap<>(employeeComparator)
+        ));
+    log.info(employeeByIdMap);
+    log.info(employeeByIdMap.getClass().getName());
+
+    assertThat(employeeByIdMap)
+        .isInstanceOf(TreeMap.class);
+  }
+
+  record Employee(int id, String name, int age, String department, int salary) {
+
+  }
+
+  private List<Employee> employees() {
+    List<Employee> employees = new ArrayList<>();
+    employees.add(new Employee(101, "Ana", 38, "IT", 50000));
+    employees.add(new Employee(102, "Ted", 40, "DM", 40000));
+    employees.add(new Employee(103, "Peter", 35, "HR", 70000));
+    employees.add(new Employee(104, "John", 38, "IT", 50000));
+    employees.add(new Employee(105, "Sam", 38, "HR", 30000));
+    employees.add(new Employee(106, "Reggie", 32, "DM", 30000));
+    employees.add(new Employee(107, "Samantha", 34, "IT", 50000));
+    employees.add(new Employee(108, "Mary", 34, "IT", 80000));
+    employees.add(new Employee(109, "Ben", 37, "IT", 60000));
+    employees.add(new Employee(110, "Valery", 34, "IT", 70000));
+    return employees;
   }
 
 }
