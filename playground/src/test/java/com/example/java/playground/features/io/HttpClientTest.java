f875c606e8af815b164849b943605bd4a9a302cd
João Luzio <joao.luzio@gmail.com>
2024-08-13 19:40:19 +0100

    Examples

--- playground/src/test/java/com/example/java/playground/features/io/HttpClientTest.java
+++ playground/src/test/java/com/example/java/playground/features/io/HttpClientTest.java
@@ -1,5 +1,8 @@
 package com.example.java.playground.features.io;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Authenticator;
@@ -10,10 +13,11 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -24,67 +28,71 @@ class HttpClientTest {
 
   @Test
   void test_get() throws URISyntaxException, IOException, InterruptedException {
-    var pageUri = new URI("https://en.wikipedia.org/wiki/Free_Willy");
+    try (HttpClient client = HttpClient.newHttpClient()) {
+      // GET
+      HttpRequest request = HttpRequest
+          .newBuilder(new URI("https://en.wikipedia.org/wiki/Free_Willy"))
+          .headers("Foo", "foovalue", "Bar", "barvalue")
+          .GET()
+          .build();
+      HttpResponse<String> response = client.send(
+          request, BodyHandlers.ofString());
+      int statusCode = response.statusCode();
+      String body = response.body();
 
-    HttpClient client = HttpClient.newHttpClient();
-
-    // GET
-    HttpResponse<String> response = client.send(
-        HttpRequest
-            .newBuilder(pageUri)
-            .headers("Foo", "foovalue", "Bar", "barvalue")
-            .GET()
-            .build(),
-        HttpResponse.BodyHandlers.ofString()
-    );
-    int statusCode = response.statusCode();
-    String body = response.body();
-
-    log.info("[get:sync] statusCode: {}{} body: {}", statusCode, System.lineSeparator(), body);
+      log.info("[get:sync] statusCode: {}{} body: {}", statusCode, System.lineSeparator(), body);
+    }
   }
 
   @Test
-  void test_get_async()
-      throws URISyntaxException, IOException, InterruptedException, ExecutionException, TimeoutException {
-    var pageUri = new URI("https://en.wikipedia.org/wiki/Free_Willy");
+  void test_get_async() throws URISyntaxException {
+    try (HttpClient client = HttpClient.newHttpClient()) {
+      // GET
+      HttpRequest request = HttpRequest
+          .newBuilder(new URI("https://en.wikipedia.org/wiki/Free_Willy"))
+          .headers("Foo", "foovalue", "Bar", "barvalue")
+          .GET()
+          .build();
+      CompletableFuture<String> responseFuture = client.sendAsync(
+              request, BodyHandlers.ofString())
+          .thenApply(HttpResponse::body);
 
-    HttpClient client = HttpClient.newHttpClient();
+      assertThat(responseFuture)
+          .isNotDone();
 
-    // GET
-    client.sendAsync(
-            HttpRequest
-                .newBuilder(pageUri)
-                .headers("Foo", "foovalue", "Bar", "barvalue")
-                .GET()
-                .build(),
-            HttpResponse.BodyHandlers.ofString()
-        )
-        .thenApply(HttpResponse::body)
-        .thenAccept(body -> log.info("[get:async] body: {}", body))
-        .get()
-    ;
+      await()
+          .until(responseFuture::isDone);
+
+      assertThat(responseFuture)
+          .isDone()
+          .isNotCompletedExceptionally();
+      // or responseFuture.get() if we wanted to wait
+      log.info("[get:async] body: {}", responseFuture.resultNow());
+    }
   }
 
   @Test
   @Disabled
-  void test_post() throws FileNotFoundException {
-    HttpClient client = HttpClient.newBuilder()
+  void test_post() throws FileNotFoundException, ExecutionException, InterruptedException {
+    try (HttpClient client = HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_1_1)
         .followRedirects(HttpClient.Redirect.NORMAL)
         .connectTimeout(Duration.ofSeconds(20))
         .proxy(ProxySelector.of(new InetSocketAddress("proxy.example.com", 80)))
         .authenticator(Authenticator.getDefault())
-        .build();
+        .build()
+    ) {
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create("https://foo.com/"))
+          .timeout(Duration.ofMinutes(2))
+          .header("Content-Type", "application/json")
+          .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("file.json")))
+          .build();
 
-    HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://foo.com/"))
-        .timeout(Duration.ofMinutes(2))
-        .header("Content-Type", "application/json")
-        .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("file.json")))
-        .build();
-
-    client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-        .thenApply(HttpResponse::body)
-        .thenAccept(System.out::println);
+      client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+          .thenApply(HttpResponse::body)
+          .thenAccept(System.out::println)
+          .get();
+    }
   }
 }
