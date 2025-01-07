db48fcfc1590b6de847f98f0a3c2e2895cb6dd86
João Luzio <joao.luzio@gmail.com>
2025-01-07 12:54:18 +0000

    Examples

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.internal.deployment.RunApplication
 
 plugins {
   java
-  id("org.springframework.boot") version "3.3.4"
-  id("io.spring.dependency-management") version "1.1.6"
+  id("org.springframework.boot") version "3.4.1"
+  id("io.spring.dependency-management") version "1.1.7"
   id("org.jsonschema2pojo") version "1.2.1"
 }
 
