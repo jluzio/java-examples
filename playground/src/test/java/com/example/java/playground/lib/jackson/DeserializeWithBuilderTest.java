e2cb5c5b9ac38fc29fba3c00ce3ac0c0ab048dc7
João Luzio <joao.luzio@gmail.com>
2025-07-29 14:04:23 +0100

    Examples

---
+++ playground/src/test/java/com/example/java/playground/lib/jackson/DeserializeWithBuilderTest.java
@@ -0,0 +1,68 @@
+package com.example.java.playground.lib.jackson;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import lombok.Builder;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {JacksonAutoConfiguration.class})
+@Log4j2
+class DeserializeWithBuilderTest {
+
+  @Autowired
+  ObjectMapper objectMapper;
+
+  @Builder
+  record SimpleData(String value) {
+
+  }
+
+  @Builder
+  @JsonDeserialize(builder = Data.DataBuilder.class)
+  record Data(String value) {
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class DataBuilder {
+
+      DataBuilder valueBase64(String value) {
+        byte[] bytes = Base64.getDecoder().decode(value);
+        String string = new String(bytes, StandardCharsets.UTF_8);
+        return value(string);
+      }
+    }
+  }
+
+  @Test
+  void test_without_builder() throws IOException {
+    var json = """
+        {"value": "test"}
+        """;
+    var data = objectMapper.readValue(json, SimpleData.class);
+    log.debug(data);
+    assertThat(data)
+        .isEqualTo(new SimpleData("test"));
+  }
+
+  @Test
+  void test_with_builder() throws IOException {
+    String valueBase64 = Base64.getEncoder().encodeToString("test".getBytes(StandardCharsets.UTF_8));
+    var json = """
+        {"valueBase64": "%s"}
+        """.formatted(valueBase64);
+    var data = objectMapper.readValue(json, Data.class);
+    log.debug(data);
+    assertThat(data)
+        .isEqualTo(new Data("test"));
+  }
+
+}
