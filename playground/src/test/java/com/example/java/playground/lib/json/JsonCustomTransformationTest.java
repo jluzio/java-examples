6ab7343c19e567beed941b279f189cc60faf3efb
João Luzio <joao.luzio@gmail.com>
2026-02-24 15:45:57 +0000

    Examples

--- playground/src/test/java/com/example/java/playground/lib/json/JsonCustomTransformationTest.java
+++ playground/src/test/java/com/example/java/playground/lib/json/JsonCustomTransformationTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.example.java.playground.lib.json.schema.JsonPatchOperation;
 import com.example.java.playground.lib.json.schema.JsonPatchOperation.CopyOperation;
 import com.example.java.playground.lib.json.schema.JsonPatchOperation.MoveOperation;
+import com.example.java.playground.lib.json.schema.JsonPatchOperation.RemoveOperation;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -40,7 +41,12 @@ class JsonCustomTransformationTest {
         {
           "id": "id1",
           "name": "name1",
-          "list": [1, 2, 3],
+          "unused": "del-me",
+          "list": ["1", "2", "3"],
+          "object_list": [
+            { "id": "id1", "value": "obj1" },
+            { "id": "id2", "value": "obj2" }
+          ],
           "map": {
             "k1": "v1",
             "k2": "v2",
@@ -52,7 +58,12 @@ class JsonCustomTransformationTest {
     var patchJson = """
         [
             {"op":"copy","from":"/id","path":"/id2"},
-            {"op":"move","from":"/name","path":"/name2"}
+            {"op":"move","from":"/name","path":"/name2"},
+            {"op":"copy","from":"/name2","path":"/list/1"},
+            {"op":"copy","from":"/object_list/0","path":"/object_first"},
+            {"op":"copy","from":"/object_list","path":"/new_object_list"},
+            {"op":"remove","path":"/unused"},
+            {"op":"remove","path":"/new_object_list/0"}
         ]
         """;
 
@@ -77,7 +88,15 @@ class JsonCustomTransformationTest {
         .hasFieldOrPropertyWithValue("id2", "id1")
         .hasFieldOrPropertyWithValue("name2", "name1")
         .doesNotContainKey("name")
+        .doesNotContainKey("unused")
     ;
+
+    assertThat(outputJsonNode.at("/object_first/id").asString())
+        .isEqualTo("id1");
+    assertThat(outputJsonNode.at("/new_object_list").size())
+        .isEqualTo(1);
+    assertThat(outputJsonNode.at("/new_object_list/0/id").asString())
+        .isEqualTo("id2");
   }
 
   public static class SimpleJsonPatch {
@@ -109,6 +128,12 @@ class JsonCustomTransformationTest {
           setPropertyContainerNode(parentTo, toPathInfo.nodeProperty(), from);
           removePropertyContainerNode(parentFrom, fromPathInfo.nodeProperty());
         }
+        case RemoveOperation op -> {
+          var toPathInfo = getPathInfo(op.path());
+          var parentTo = getParentContainerNode(root, toPathInfo);
+
+          removePropertyContainerNode(parentTo, toPathInfo.nodeProperty());
+        }
         default -> throw new IllegalStateException("Unexpected value: " + patch);
       }
     }
@@ -120,13 +145,17 @@ class JsonCustomTransformationTest {
     }
 
     private ContainerNode<?> getParentContainerNode(JsonNode root, JsonNodePathInfo pathInfo) {
-      return (ContainerNode<?>) root.at(pathInfo.parentPath());
+      var parentNode = root.at(pathInfo.parentPath());
+      if (parentNode instanceof ContainerNode<?> node) {
+        return node;
+      }
+      throw new IllegalStateException("Unable to get parent ContainerNode for path: " + pathInfo.path());
     }
 
     private void setPropertyContainerNode(ContainerNode<?> containerNode, String nodeProperty, JsonNode jsonNode) {
       switch (containerNode) {
         case ObjectNode node -> node.set(nodeProperty, jsonNode);
-        case ArrayNode node -> node.set(Integer.parseInt(nodeProperty), jsonNode);
+        case ArrayNode node -> node.set(getArrayIndex(nodeProperty), jsonNode);
         default -> throw new IllegalStateException("Unexpected value: " + containerNode);
       }
     }
@@ -134,9 +163,13 @@ class JsonCustomTransformationTest {
     private void removePropertyContainerNode(ContainerNode<?> containerNode, String nodeProperty) {
       switch (containerNode) {
         case ObjectNode node -> node.remove(nodeProperty);
-        case ArrayNode node -> node.remove(Integer.parseInt(nodeProperty));
+        case ArrayNode node -> node.remove(getArrayIndex(nodeProperty));
         default -> throw new IllegalStateException("Unexpected value: " + containerNode);
       }
+    }
+
+    private int getArrayIndex(String nodeProperty) {
+      return Integer.parseInt(nodeProperty);
     }
   }
 }
