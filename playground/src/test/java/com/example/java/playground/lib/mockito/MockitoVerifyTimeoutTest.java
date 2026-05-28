1c53014addd4b366420d7d768ed89724a1a41cad
João Luzio <joao.luzio@gmail.com>
2026-05-28 19:17:59 +0100

    Examples

--- playground/src/test/java/com/example/java/playground/lib/mockito/MockitoVerifyTimeoutTest.java
+++ playground/src/test/java/com/example/java/playground/lib/mockito/MockitoVerifyTimeoutTest.java
@@ -6,6 +6,8 @@ import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -13,18 +15,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 
 @ExtendWith(MockitoExtension.class)
+@Slf4j
 class MockitoVerifyTimeoutTest {
 
   @Mock
   Runnable signal;
 
   @Test
+  @SuppressWarnings("java:S5778")
   void timeout_testing() {
-    Runnable task = () -> {
-      Mono.delay(Duration.ofMillis(50L))
-          .doOnNext(_ -> signal.run())
-          .subscribe();
-    };
+    Runnable task = () -> CompletableFuture.runAsync(() ->
+        Mono.delay(Duration.ofMillis(50L))
+            .doOnNext(_ -> signal.run())
+            .doOnNext(_ -> log.info("done"))
+            .block());
 
     task.run();
     verify(signal, timeout(100L)).run();
